f8f2ba7cc82ad80486af707b4889391e9f36982e
RomboutKarelse <86190188+RomboutKarelse@users.noreply.github.com>
2023-07-22 17:29:21 +0200

    Create prepare.py

---
+++ data/shakespeare/prepare.py
@@ -0,0 +1,30 @@
+# inspired by https://github.com/karpathy/nanoGPT
+import os
+import requests
+from transformers import GPT2TokenizerFast
+
+# download the tiny shakespeare dataset
+input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+if not os.path.exists(input_file_path):
+    data_url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    with open(input_file_path, 'w') as f:
+        f.write(requests.get(data_url).text)
+
+with open(input_file_path, 'r') as f:
+    data = f.read()
+n = len(data)
+train_data = data[:int(n*0.9)]
+val_data = data[int(n*0.9):]
+
+# encode with Huggingface tokenizer
+tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
+train_ids = tokenizer.encode_ordinary(train_data)
+val_ids = tokenizer.encode_ordinary(val_data)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
+
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
